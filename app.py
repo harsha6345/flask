5fe7ac1eaca4c09c6d8f72783c7e64b16474121c
harsha <harshab6345@gmail.com>
2023-07-22 17:13:09 +0530

    set up some routes

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+@app.route('/products')
+def products():
+    return "This is a products page"
+
 if __name__ == "__main__" :
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
